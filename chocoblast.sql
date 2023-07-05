cfff7189a297b20b276fb7fc7baa2b9bb507f453
leylanajmaya31 <leylasalmi86@gmail.com>
2023-07-05 14:58:32 +0200

    ajout-de-cours

--- chocoblast.sql
+++ chocoblast.sql
@@ -1,39 +1,34 @@
-create database choco;
-use choco;
-
-create table roles(
-id_roles int primary key auto_increment not null,
-nom varchar(50) not null
-)Engine=InnoDB;
-
-create table utilisateur(
+create database chocoblast;
+use chocoblast;
+create table utilisateur (
 id_utilisateur int primary key auto_increment not null,
-nom varchar(50) not null,
-prenom varchar(50) not null,
-email varchar(50) not null,
-password varchar(100) not null,
-image varchar(50) not null,
-statut tinyint(1) default 0,
+nom_utilisateur varchar(50) not null,
+prenom_utilisateur varchar(50) not null,
+email_utilisateur varchar(50) not null,
+password_utilisateur varchar (100) not null,
+image_utilisateur varchar (100) not null,
+statut_utilisateur tinyint(1) not null,
 id_roles int not null
 )Engine=InnoDB;
 
-create table chocoblast(
+create table chocoblast (
 id_chocoblast int primary key auto_increment not null,
-slogan text not null,
-date_creation date not null,
-statut tinyint(1) default 0,
-auteur int not null,
-cible int not null
+slogan_chocoblast text not null,
+date_creation_chocoblast datetime not null,
+statut_chocoblast tinyint(1) not null
+)Engine=InnoDB;
+-- alter table chocoblast change slogan_chocblast slogan_chocoblast text;
+create table roles (
+id_roles int primary key auto_increment not null,
+nom_roles varchar(50) not null
 )Engine=InnoDB;
 
-create table commentaire(
+create table commentaire (
 id_commentaire int primary key auto_increment not null,
-note int not null,
-contenu text not null,
-date_creation datetime not null,
-statut tinyint(1) default 0,
-id_chocoblast int not null,
-auteur int not null
+note_commentaire int not null,
+contenu_commentaire text,
+date_creation_commentaire datetime not null,
+statut_chocoblast tinyint(1) not null
 )Engine=InnoDB;
 
 alter table utilisateur
@@ -41,22 +36,20 @@ add constraint fk_posseder_roles
 foreign key(id_roles)
 references roles(id_roles);
 
-alter table chocoblast
-add constraint fk_chocoblaster_utilisateur
-foreign key(auteur)
-references utilisateur(id_utilisateur);
-
-alter table chocoblast
-add constraint fk_cibler_utilisateur
-foreign key(cible)
-references utilisateur(id_utilisateur);
-
 alter table commentaire
-add constraint fk_poster_utilisateur
-foreign key(auteur)
-references utilisateur(id_utilisateur);
-
-alter table commentaire
-add constraint fk_rattacher_chocoblast
+add column id_chocoblast int,
+add column id_auteur int,
+add constraint fk_poster_chocoblast
 foreign key(id_chocoblast)
 references chocoblast(id_chocoblast);
+
+
+alter table chocoblast
+add column id_auteur int not null,
+add column id_cible int not null,
+add constraint fk_rattaccher_chocoblast
+foreign key(id_cible)
+references utilisateur(id_utilisateur),
+add constraint fk_auteur_chocoblast
+foreign key(id_auteur)
+references utilisateur(id_utilisateur);
--- exo_sql_shop.sql
+++ exo_sql_shop.sql
@@ -52,57 +52,5 @@ add constraint FK_receipt
 foreign key (id_receipt)
 references receipt(id_receipt);
 
-create database chocoblast;
-use chocoblast;
-create table utilisateur (
-id_utilisateur int primary key auto_increment not null,
-nom_utilisateur varchar(50) not null,
-prenom_utilisateur varchar(50) not null,
-email_utilisateur varchar(50) not null,
-password_utilisateur varchar (100) not null,
-image_utilisateur varchar (100) not null,
-statut_utilisateur tinyint(1) not null,
-id_roles int not null
-)Engine=InnoDB;
 
-create table chocoblast (
-id_chocoblast int primary key auto_increment not null,
-slogan_chocoblast text not null,
-date_creation_chocoblast datetime not null,
-statut_chocoblast tinyint(1) not null
-)Engine=InnoDB;
--- alter table chocoblast change slogan_chocblast slogan_chocoblast text;
-create table roles (
-id_roles int primary key auto_increment not null,
-nom_roles varchar(50) not null
-)Engine=InnoDB;
-
-create table commentaire (
-id_commentaire int primary key auto_increment not null,
-note_commentaire int not null,
-contenu_commentaire text,
-date_creation_commentaire datetime not null,
-statut_chocoblast tinyint(1) not null
-)Engine=InnoDB;
-
-alter table utilisateur
-add constraint fk_posseder_roles
-foreign key (id_roles)
-references roles(id_roles);
-
-alter table commentaire
-add constraint fk_poster_chocoblast
-foreign key(id_chocoblast)
-references chocoblast(id_chocoblast);
-
-
-alter table chocoblast
-add column id_auteur int not null,
-add column id_cible int not null,
-add constraint fk_rattaccher_chocoblast
-foreign key(id_cible)
-references utilisateur(id_utilisateur),
-add constraint fk_auteur_chocoblast
-foreign key(id_auteur)
-references utilisateur(id_utilisateur);
 
